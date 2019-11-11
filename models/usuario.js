7f77ebc48cbdb3707521e851864e55a85cb34bc4
Fredy Fuentes <fredy.fuentesb@gmail.com>
2019-11-11 12:49:02 -0500

    Usuario y Token listo

---
+++ models/usuario.js
@@ -0,0 +1,21 @@
+var moongose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = moongose.Schema;
+
+var rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol permitido'
+};
+
+var usuarioSchema = new Schema({
+    nombre: { type: String, required: [true, 'El nombre es necesario'] },
+    email: { type: String, unique: true, required: [true, 'El email es necesario'] },
+    password: { type: String, required: [true, 'La contraseña es necesaria'] },
+    img: { type: String, required: false },
+    rol: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
+});
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico' })
+
+module.exports = moongose.model('usuario', usuarioSchema);
